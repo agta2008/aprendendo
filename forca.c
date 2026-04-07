f5735fb758b38a196d99a28634718bca513e9d5e
agta2008 <agta2008.avila@gmail.com>
2026-04-07 18:51:39 -0300

    finalizei o jogo de forca, agora sou feliz

--- forca.c
+++ forca.c
@@ -6,7 +6,7 @@
 
 //variaveis globais
 char chutes[26];
-char palavrasecreta[20];
+char palavrasecreta[TAMANHO_PALAVRA];
 int chutesdados = 0;
 
 
@@ -38,32 +38,103 @@ int jachutou(char letra){
     return achou;
 }
 
+int letraexiste(char letra) {
+
+    for(int j = 0; j < strlen(palavrasecreta); j++) {
+        if(letra == palavrasecreta[j]) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+
+int chuteserrados() {
+    int erros = 0;
+
+    for(int i = 0; i < chutesdados; i++) {
+
+        if(!letraexiste(chutes[i])) {
+            erros++;
+        }
+    }
+
+    return erros;
+}
 
 void desenhaforca() {
-    for (int i = 0; i < strlen(palavrasecreta); i++) {
 
-        //aqui estava o codigo que verifica se achou a letra certa
-        // essa funcao retorna {achou} paasando a funcao jachutou
-        int achou = jachutou(palavrasecreta[i]);
+    int erros = chuteserrados();
 
-        if(achou) {
-                
+    printf("  _______       \n");
+    printf(" |/      |      \n");
+    printf(" |      %c%c%c  \n", (erros>=1?'(':' '), (erros>=1?'_':' '), (erros>=1?')':' '));
+    printf(" |      %c%c%c  \n", (erros>=3?'\\':' '), (erros>=2?'|':' '), (erros>=3?'/': ' '));
+    printf(" |       %c     \n", (erros>=2?'|':' '));
+    printf(" |      %c %c   \n", (erros>=4?'/':' '), (erros>=4?'\\':' '));
+    printf(" |              \n");
+    printf("_|___           \n");
+    printf("\n\n");
+
+    for(int i = 0; i < strlen(palavrasecreta); i++) {
+
+        if(jachutou(palavrasecreta[i])) {
             printf("%c ", palavrasecreta[i]);
-            
         } else {
             printf("_ ");
         }
-            
+
     }
     printf("\n");
+
 }
+
+
+void adicionapalavra() {
+    char quer;
+    printf("\n Voce quer adicionar uma nova palavra no banco de dados? (S/N) ");
+    scanf(" %c", &quer);
+
+    if (quer == 'S') {
+        char novapalavra[29];
+
+        printf("Qual a nova palavra voce quer? ");
+        scanf("%s", novapalavra);
+
+        FILE* f;
+        f = fopen("palavras.txt", "r+");
+        if (f == 0) {
+            printf("Desclpa banco de dados nao disponivel \n\n");
+            exit(1);            
+        }
+
+
+        int qtd;
+        fscanf(f, "%d", &qtd);
+        qtd++;
+
+        fseek(f, 0, SEEK_SET);
+        fprintf(f, "%d", qtd);
+
+        fseek(f, 0,SEEK_END);
+        fprintf(f, "\n%s", novapalavra);
+        fclose(f);
+
+    } else {
+        printf("\n  OK  \n");
+        exit(1);
+    }
+}
+
+
 
 void escolhapalavrasecreta() {
     FILE* f;
     f = fopen("palavras.txt", "r");
 
     if(f==0) {
-        printf("Desclpa banco de dados nao disponivel");
+        printf("Desclpa banco de dados nao disponivel \n\n");
         exit(1);
     }
 
@@ -90,7 +161,14 @@ int ganhou() {
 }
 
 
+// AQUI 
+
+
+
 int enforcou() {
+
+    //COLOCAR A FUNCAO JACHUTOU
+
     int erros = 0;
     for(int i = 0; i < chutesdados; i++) {
         int existe = 0;
@@ -125,5 +203,17 @@ int main() {
         chuta(); 
         
     } while(!ganhou() && !enforcou() );
-
+    
+    if (ganhou()) {
+        printf("Parabens voce acertou a palavra secreta era, %s", palavrasecreta);
+    } else {
+        printf("Voce atinjiu a quantidade maxima de chutes \n\n");
+        printf("Sinto muito a palavra secreta era **%s**", palavrasecreta);
+    }
+    
+    
+    
+    
+    
+    adicionapalavra();
 }
--- forca.h
+++ forca.h
@@ -1,3 +1,8 @@
+#define TAMANHO_PALAVRA 20
+
+int letraexiste(char letra);
+int chuteserrados();
+void desenhaforca();
 void abertura();
 void chuta();
 int jachutou(char letra);
@@ -5,3 +10,4 @@ void desenhaforca();
 void escolhapalavrasecreta();
 int ganhou();
 int enforcou();
+void adicionapalavra();
